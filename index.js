265f6b3bf7f1fd90748f38809fd693a1b1a7627e
Gihan Ubayawardana <gihan.ubayawardana@infor.com>
2019-06-16 01:33:04 +0530

    updated index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-module.exports = function tiny(string) {
+const tiny = (string) => {
     if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
     return string.replace(/\s/g, "");
-};
+}
+
+export default tiny;
